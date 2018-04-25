b4efb87139fdea342653b5298894e9d8dfe0953f
xwb <gylu20@163.com>
2018-04-25 19:47:26 +0800

    first program

---
+++ xwbrep/xwb01-ecom-cloud/src/main/java/com/xwb01/ecom/cloud/xwb01_ecom_cloud/App.java
@@ -0,0 +1,13 @@
+package com.xwb01.ecom.cloud.xwb01_ecom_cloud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
